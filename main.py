7682ea3bb38ef336dc6dffdf0f758b46e9b9a806
Ray <105521700+Barrow75@users.noreply.github.com>
2025-01-31 22:36:07 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,42 @@
+# Prompt: Write a function that takes an ordered list of numbers (a list where the elements are in order
+# from smallest to largest) and another number. The function decides whether or not
+# the given number is inside the list and returns (then prints) an appropriate boolean.
+
+
+def OrderList():
+    order = input("Enter Numbers: ").split()
+    new = list(map(int, order))
+    new.sort()
+
+    return new
+
+new = OrderList()
+print(new)
+
+def BinarySearch(new):
+
+    check_list = int(input("Is this number in the list: "))
+    start_index = 0
+    end_index = len(new) - 1
+
+    while True:
+
+        if start_index > end_index:
+            return -1
+
+        half_list = (end_index + start_index) // 2
+
+        if check_list == new[int(half_list)]:
+            return half_list
+
+        elif check_list > new[int(half_list)]:
+            start_index = half_list + 1
+
+        else:
+            end_index = half_list - 1
+
+
+
+
+result = BinarySearch(new)
+print(result)
